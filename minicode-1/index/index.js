6b6abb85a24c26b8ebab193a84e7d25acc0a393f
Qiuhang817385 <826266129@qq.com>
2020-08-10 23:20:11 +0800

    周一 8月 10 10:13 下午复习小程序

--- minicode-1/index/index.js
+++ minicode-1/index/index.js
@@ -1,8 +1,20 @@
 Page({
-  onLoad() {
+  data: {
+    val: '',
+    todos: ['1', '2', '3']
+  },
+  // 这样的话可以异步添加List数据
+  addTodo () {
+    this.setData({
+      val: '',
+      todos: [...this.data.todos, this.data.val]
+    })
+  },
+  onLoad () {
+    // 创建摄像头
     this.ctx = wx.createCameraContext()
   },
-  takePhoto() {
+  takePhoto () {
     this.ctx.takePhoto({
       quality: 'high',
       success: (res) => {
@@ -12,14 +24,14 @@ Page({
       }
     })
   },
-  startRecord() {
+  startRecord () {
     this.ctx.startRecord({
       success: (res) => {
         console.log('startRecord')
       }
     })
   },
-  stopRecord() {
+  stopRecord () {
     this.ctx.stopRecord({
       success: (res) => {
         this.setData({
@@ -29,7 +41,7 @@ Page({
       }
     })
   },
-  error(e) {
+  error (e) {
     console.log(e.detail)
   }
 })
